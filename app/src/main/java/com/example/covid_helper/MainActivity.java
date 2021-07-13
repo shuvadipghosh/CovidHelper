0e7d034cdf3deb852a45baeb2a0d7329b276d451
Shuvadip Ghosh <shuvadipghos055@gmail.com>
2021-07-13 09:50:12 +0530

    first version

---
+++ app/src/main/java/com/example/covid_helper/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.covid_helper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View decorView=getWindow().getDecorView();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    |View.SYSTEM_UI_FLAG_FULLSCREEN
+                    |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
+    }
+}
